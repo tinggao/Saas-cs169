f6449ba8727695098e7c35babeb6a4a4ac748d2c
tinggao <tingting-gao@uiowa.edu>
2014-05-31 13:50:20 -0500

    update hw1

---
+++ hw1-3.rb
@@ -0,0 +1,13 @@
+class BookInStock
+	
+	def initialize(isbn,price)
+		raise ArgumentError if (isbn.empty? || price<0)
+		@isbn=isbn
+		@price=price
+	end
+	def self.price_as_string(price)
+		"$%.2f"%price
+	end
+end
+p BookInStock.new("98098",3.45)
+p BookInStock.new("",3.45)
